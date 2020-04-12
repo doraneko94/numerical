d17c3a0f9b07fdbb75d0a55665923817facd0700
doraneko94 <shuntaro94@gmail.com>
2020-04-13 01:01:05 +0900

    add NewtonDivItp

--- examples/interpolation.rs
+++ examples/interpolation.rs
@@ -12,4 +12,33 @@ fn main() {
     println!("f(2.0) = {:.5}", i.calc(2.0));
     println!("f(0.8) = {:.5}", i.calc(0.8));
     println!("");
+
+    let mut i: LagrangeItp<f64> = LagrangeItp::new(&vec![   0.5,    1.5], 
+                                                   &vec![0.3734, 0.4712]);
+    i.push_vec(&vec![   1.0,    2.0], 
+               &vec![0.5104, 0.3345]);
+    println!("x = [   0.5,    1.5,    1.0,    2.0]");
+    println!("y = [0.3734, 0.4712, 0.5104, 0.3345]");
+    println!("f(0.5) = {:.5}", i.calc(0.5));
+    println!("f(1.0) = {:.5}", i.calc(1.0));
+    println!("f(1.5) = {:.5}", i.calc(1.5));
+    println!("f(2.0) = {:.5}", i.calc(2.0));
+    println!("f(0.8) = {:.5}", i.calc(0.8));
+    println!("");
+
+    println!("*** Newton's Divided Difference interpolation ***");
+    let i: NewtonDivItp<f64> = NewtonDivItp::new(&vec![   0.2,    0.5,    1.0,    1.5,    2.0,    3.0], 
+                                                 &vec![0.0793, 0.1915, 0.3413, 0.4332, 0.4772, 0.4987]);
+    println!("x = [   0.2,    0.5,    1.0,    1.5,    2.0,    3.0]");
+    println!("y = [0.0793, 0.1915, 0.3413, 0.4332, 0.4772, 0.4987]");
+    println!("f(0.7) = {:.5}", i.calc(0.7));
+    println!("");
+
+    let mut i: NewtonDivItp<f64> = NewtonDivItp::new(&vec![   0.2,    1.0,    2.0], 
+                                                     &vec![0.0793, 0.3413, 0.4772]);
+    i.push_vec(&vec![   0.5,    1.5,    3.0], 
+               &vec![0.1915, 0.4332, 0.4987]);
+    println!("x = [   0.2,    1.0,    2.0,    0.5,    1.5,    3.0]");
+    println!("y = [0.0793, 0.3413, 0.4772, 0.1915, 0.4332, 0.4987]");
+    println!("f(0.7) = {:.5}", i.calc(0.7));
 }
--- src/interpolation.rs
+++ src/interpolation.rs
@@ -27,6 +27,9 @@ impl<F: Float> Itp<F> for LagrangeItp<F> {
     }
 
     fn push_vec(&mut self, x: &Vec<F>, y: &Vec<F>) {
+        if x.len() != y.len() {
+            panic!("x and y have different number of elements!");
+        }
         for (&xi, &yi) in x.iter().zip(y.iter()) {
             self.x.push(xi);
             self.y.push(yi);
@@ -43,6 +46,60 @@ impl<F: Float> Itp<F> for LagrangeItp<F> {
                 }
             }
             ans = ans + l * yi;
+        }
+        ans
+    }
+}
+
+pub struct NewtonDivItp<F: Float> {
+    pub x: Vec<F>,
+    pub y: Vec<F>,
+    pub diff: Vec<Vec<F>>,
+}
+
+impl<F: Float> Itp<F> for NewtonDivItp<F> {
+    fn new(x: &Vec<F>, y: &Vec<F>) -> Self {
+        let n = x.len();
+        if n != y.len() {
+            panic!("x and y have different number of elements!");
+        }
+        let x = x.clone();
+        let y = y.clone();
+        let mut diff: Vec<Vec<F>> = vec![(0..n-1).map(|i| (y[i] - y[i+1]) / (x[i] - x[i+1])).collect()];
+        for i in 1..n-1 {
+            diff.push((0..n-i-1).map(|j|  (diff[i-1][j] - diff[i-1][j+1]) / (x[j] - x[j+i+1])).collect());
+        }
+        Self { x, y, diff }
+    }
+
+    fn push(&mut self, x: F, y: F) {
+        let n = self.x.len();
+        self.x.push(x);
+        self.y.push(y);
+        self.diff[0].push((self.y[n-1] - self.y[n]) / (self.x[n-1] - self.x[n]));
+        for i in 1..n-1 {
+            let d_diff = self.diff[i-1][n-1-i] - self.diff[i-1][n-i];
+            self.diff[i].push(d_diff / (self.x[n-i-1] - self.x[n]));
+        }
+        let d_diff = self.diff[n-2][0] - self.diff[n-2][1];
+        self.diff.push(vec![d_diff / (self.x[0] - self.x[n])]);
+    }
+
+    fn push_vec(&mut self, x: &Vec<F>, y: &Vec<F>) {
+        if x.len() != y.len() {
+            panic!("x and y have different number of elements!");
+        }
+        for (&xi, &yi) in x.iter().zip(y.iter()) {
+            self.push(xi, yi);
+        }
+    }
+
+    fn calc(&self, z: F) -> F {
+        let mut ans: F = self.y[0];
+        let mut m: F = One::one();
+        for i in 0..self.diff.len() {
+            m = m * (z - self.x[i]);
+            ans = ans + m * self.diff[i][0];
         }
         ans
     }
